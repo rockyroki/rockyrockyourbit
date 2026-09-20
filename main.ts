6c64c4e0eaf92e1764f1ec45728052392650edac
rockyroki <148733983+rockyroki@users.noreply.github.com>
2026-09-20 11:27:04 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-        music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.UntilDone)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.InBackground)
         for (let index = 0; index < 2; index++) {
             basic.showLeds(`
                 . . # . .
@@ -25,14 +18,7 @@ radio.onReceivedNumber(function (receivedNumber) {
                 `)
         }
     } else if (receivedNumber == 2) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.InBackground)
         for (let index = 0; index < 2; index++) {
             basic.showLeds(`
                 . . # . .
@@ -50,13 +36,6 @@ radio.onReceivedNumber(function (receivedNumber) {
                 `)
         }
     } else if (receivedNumber == 3) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
         music.play(music.createSoundExpression(WaveShape.Square, 200, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
         for (let index = 0; index < 2; index++) {
             basic.showLeds(`
@@ -74,40 +53,31 @@ radio.onReceivedNumber(function (receivedNumber) {
                 . . . . .
                 `)
         }
-    } else if (false) {
-        if (receivedNumber == 4) {
-            music.play(music.stringPlayable("C5 G C5 G C5 G C5 G ", 120), music.PlaybackMode.UntilDone)
-            for (let index = 0; index < 4; index++) {
-                basic.showLeds(`
-                    . # . # .
-                    # # # # #
-                    # # # # #
-                    . # # # .
-                    . . # . .
-                    `)
-                basic.showLeds(`
-                    . . . . .
-                    . # . # .
-                    . # # # .
-                    . . # . .
-                    . . . . .
-                    `)
-            }
+    } else if (receivedNumber == 4) {
+        basic.clearScreen()
+        music.play(music.stringPlayable("C5 G C5 G C5 G C5 G ", 120), music.PlaybackMode.InBackground)
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . # . # .
+                # # # # #
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
         }
     } else {
         music.stopAllSounds()
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(5)
-    music.play(music.stringPlayable("C D E C E G A G ", 170), music.PlaybackMode.InBackground)
-    basic.showString("safeRIDE")
 })
 radio.setGroup(610)
